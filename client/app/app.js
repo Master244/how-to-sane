1fc30ab85dee0483eaa36f95fbb87eac11b7a574
Martin Genev <mgenev@gmail.com>
2015-04-21 11:41:10 -0700

    small clean up of app.js

--- client/app/app.js
+++ client/app/app.js
@@ -1,9 +1,8 @@
-/* jshint unused: false */
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
-import Route from './overrides/route';
+import './overrides/route';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
